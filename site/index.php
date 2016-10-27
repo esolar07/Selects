263ace771a0b657ab684a1e0b36e5bda2e2fe61b
Eddie Solar <e_solar@mgcorp.co>
2016-10-27 17:32:04 -0400

    started Selects php site. added slim framework.

---
+++ site/index.php
@@ -0,0 +1,17 @@
+<?
+
+require __DIR__ . '/vendor/autoload.php';
+
+date_default_timezone_set('America/New_York');
+
+
+//use Monolog\Logger;
+//use Monolog\Handler\StreamHandler;
+
+//$log = new Logger('name');
+//$log->pushHandler(new StreamHandler('app.log', Logger::WARNING));
+//$log->addWarning('Foo');
+
+
+?>
+
